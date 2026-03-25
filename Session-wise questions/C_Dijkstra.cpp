c9d568f32b988a9ded12433c7f8819b4ca4bfdd2
Jashanpreet Kaur <23BCS14043@cuchd.in>
2026-03-25 12:35:31 +0530

    Implemented Dijkstra, Bellman ford and Floyd Warshell in the session

---
+++ Session-wise questions/C_Dijkstra.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<pair<int,int>>> graph(n);
+    for(int i=0; i<m; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        graph[u].push_back({v,w});
+        graph[v].push_back({u,w});
+    }
+    int src=1;
+    vector<int> dist(n, INT_MAX);
+    dist[src]=0;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    pq.push({0,src});
+    while(!pq.empty()){
+        auto [d,u]=pq.top();
+        pq.pop();
+        if(d>dist[u]) continue;
+        for(auto [v,w]: graph[u]){
+            if(dist[u]!=INT_MAX && dist[u]+w<dist[v]){
+                dist[v]=dist[u]+w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<dist[i]<<" ";
+    }
+
+}
